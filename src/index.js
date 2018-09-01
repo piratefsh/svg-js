8351375714cf9be3e365c507b69cf345f69f2a80
Sher Minn Chong <sherminn.chong@gmail.com>
2018-09-01 11:56:31 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const pluginsTriggerElement = document.getElementById('plugins-trigger');
+    const pluginsElement = document.getElementById('plugins');
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    }
+});
