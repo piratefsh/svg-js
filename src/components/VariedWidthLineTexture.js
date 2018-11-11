7e042ca42697ded3b8979bac2cc265ef7c12b745
Sher Minn Chong <sherminn.chong@gmail.com>
2018-11-11 17:59:07 -0500

    Render multiple textures at once

--- src/components/VariedWidthLineTexture.js
+++ src/components/VariedWidthLineTexture.js
@@ -1,11 +1,14 @@
 import Texture from "./Texture";
 
-export default class LineTexture extends Texture {
+export default class VariedWidthLineTexture extends Texture {
     draw() {
         this.ctx.draw(() => {
             this.ctx.setStyles(this.styles);
 
             for (let i = 0; i < this.numStrokes; i += 1) {
+                this.ctx.setStyles({
+                    strokeWidth: Math.floor(1 + Math.random() * 5)
+                })
                 const x = Math.floor(Math.random() * this.ctx.width);
                 const y1 = Math.random() > 0.5 ? 0 : this.ctx.height;
                 const y2 = Math.floor(Math.random() * this.ctx.height);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "./styles/index.scss";
 import { makeContextContainer, makeSaveButton } from "./helpers/dom-helpers";
+import VariedWidthLineTexture from "./components/VariedWidthLineTexture";
 import LineTexture from "./components/LineTexture";
 import CircleTexture from "./components/CircleTexture";
 import RectTexture from "./components/RectTexture";
 import P5Context from "./contexts/P5Context";
 import SVGContext from "./contexts/SVGContext";
 
-const makeDrawing = ({ Context, width, height, drawingOptions }) => {
+const makeDrawing = ({
+    Context,
+    width,
+    height,
+    drawingOptions,
+    texture: TextureClass
+}) => {
     const parent = makeContextContainer(Context.NAME);
     const ctx = new Context(parent, width, height);
     const options = {
@@ -17,9 +24,7 @@ const makeDrawing = ({ Context, width, height, drawingOptions }) => {
 
     Object.assign(options, drawingOptions);
 
-    const instance = new LineTexture(options);
-    // const instance = new RectTexture(options);
-    // const instance = new CircleTexture(options);
+    const instance = new TextureClass(options);
     instance.draw();
     makeSaveButton({
         label: `Save ${Context.name}`,
@@ -31,19 +36,28 @@ const main = () => {
     const width = 300;
     const height = 300;
 
-    for (let i = 100; i < 1800; i += 200) {
-        makeDrawing({
-            Context: P5Context,
-            width,
-            height,
-            drawingOptions: {
-                numStrokes: i,
-                styles: {
-                    strokeWidth: 2,
-                    stroke: "hsla(0, 0%, 30%, 0.3)"
+    const textures = [
+        VariedWidthLineTexture,
+        LineTexture,
+        RectTexture,
+        CircleTexture
+    ];
+    for (let n = 0; n < textures.length; n++) {
+        for (let i = 200; i < 1200; i += 400) {
+            makeDrawing({
+                Context: P5Context,
+                width,
+                height,
+                texture: textures[n],
+                drawingOptions: {
+                    numStrokes: i,
+                    styles: {
+                        strokeWidth: 2,
+                        stroke: "hsla(0, 0%, 30%, 0.3)"
+                    }
                 }
-            }
-        });
+            });
+        }
     }
     // makeDrawing({ Context: SVGContext, width, height });
 };
