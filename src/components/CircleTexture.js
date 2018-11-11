ced515c62725f14441afc6ed63367558737792bc
Sher Minn Chong <sherminn.chong@gmail.com>
2018-11-11 14:58:13 -0500

    Abstract out Texture class

--- src/components/CircleTexture.js
+++ src/components/CircleTexture.js
@@ -1,40 +1,18 @@
-export default class CircleTexture {
-    constructor({ styles, ctx, width, height, numStrokes }) {
-        // add defaults
-        this.styles = Object.assign(
-            {
-                stroke: "black",
-                strokeWidth: 1,
-                fill: "rgba(0,0,0,0)"
-            },
-            styles
-        );
+import Texture from "./Texture";
 
-        // set drawing contesxt
-        this.ctx = ctx;
-
-        this.width = width;
-        this.height = height;
-
-        this.numStrokes = numStrokes || 500;
-    }
-
+export default class CircleTexture extends Texture {
     draw() {
         this.ctx.draw(() => {
             this.ctx.setStyles(this.styles);
 
             for (let i = 0; i < this.numStrokes; i += 1) {
                 this.ctx.ellipse(
-                    (i / this.numStrokes) * this.width*1.5,
-                    (i / this.numStrokes) * this.height*1.5,
+                    (i / this.numStrokes) * this.width * 1.5,
+                    (i / this.numStrokes) * this.height * 1.5,
                     Math.random() * this.ctx.width,
                     Math.random() * this.ctx.height
                 );
             }
         });
-    }
-
-    save() {
-        this.ctx.save();
     }
 }
