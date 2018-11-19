5ca913c3f78c9364ff70e6d1f75894213d0b1475
Sher Minn Chong <sherminn.chong@gmail.com>
2018-11-18 23:50:37 -0500

    Fix linting only

--- src/components/RibbonTexture.js
+++ src/components/RibbonTexture.js
@@ -6,26 +6,31 @@ export default class CircleTexture extends Texture {
             this.ctx.setStyles(this.styles);
             const { width, height } = this;
             const [x, y] = [this.translate.x, this.translate.y];
-            console.log(width, height)
-            console.log(this.translate)
+            console.log(width, height);
+            console.log(this.translate);
             const curve = [
                 this.translate.x + Math.random() * width,
                 this.translate.y + Math.random() * height,
-                this.translate.x + Math.random() * width/2,
+                this.translate.x + (Math.random() * width) / 2,
                 this.translate.y + Math.random() * height,
                 x,
                 height
             ];
             for (let i = 0; i < width; i += 2) {
                 const offsetX = i;
-                this.ctx.cubicBezier([x + offsetX, y], [[
-                    curve[0]+ i,
-                    curve[1]+ i,
-                    curve[2]+ i,
-                    curve[3]+ i,
-                    x+i,
-                    this.translate.y + height,
-                    ]]);
+                this.ctx.cubicBezier(
+                    [x + offsetX, y],
+                    [
+                        [
+                            curve[0] + i,
+                            curve[1] + i,
+                            curve[2] + i,
+                            curve[3] + i,
+                            x + i,
+                            this.translate.y + height
+                        ]
+                    ]
+                );
             }
         });
     }
