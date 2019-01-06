f27c177826baac5a9446a9573d44859e4154217d
Sher Minn Chong <sherminn.chong@gmail.com>
2019-01-06 14:17:14 -0500

    Implement line for p5

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -24,15 +24,19 @@ export default class Drawing {
             ctx.rect(this.width, this.height, 0, 0);
 
             ctx.setStyles(this.styles);
-            ctx.startBezier(30, 70);
-            ctx.bezierVertex(25, 25, 100, 50, 50, 100);
-            ctx.bezierVertex(75, 140, 120, 120);
-            ctx.endBezier();
 
             ctx.startBezier(30, 70);
             ctx.bezierVertex(50, 50, 134, 122, 160, 170);
             ctx.bezierVertex(190, 120, 210, 210);
             ctx.endBezier();
+
+            ctx.startLine();
+            ctx.lineVertex(200, 0);
+            ctx.lineVertex(100, 50);
+            ctx.lineVertex(250, 100);
+            ctx.lineVertex(100, 200);
+            ctx.lineVertex(400, 400);
+            ctx.endLine();
         });
     }
 
