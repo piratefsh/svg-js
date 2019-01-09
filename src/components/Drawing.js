f2b0921646311273654f810ae3a22c8480994c2f
Sher Minn Chong <sherminn.chong@gmail.com>
2019-01-09 15:54:05 -0500

    Cleanup and fix bug

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -18,8 +18,10 @@ export default class Drawing {
         this.width = width;
         this.height = height;
 
-        this.gridX = 6;
-        this.gridY = 6;
+        this.gridX = 3;
+        this.gridY = 3;
+
+        this.linePerCell = 20;
 
         this.gutter = 24;
     }
@@ -30,25 +32,42 @@ export default class Drawing {
 
     randTrapezium({ x: sx, y: sy }, { x: ex, y: ey }) {
         const { jitter } = this;
-        const yJitter = jitter(20);
-        return [
+        const vertices = [
             {
                 x: sx,
-                y: sy + yJitter
+                y: sy
             },
             {
-                x: ex - jitter(15),
-                y: sy + yJitter
+                x: ex,
+                y: sy
             },
             {
-                x: ex - jitter(),
+                x: ex,
                 y: ey
             },
             {
-                x: sx + jitter(15),
-                y: ey - jitter(15)
+                x: sx,
+                y: ey
             }
         ];
+
+        // pick 2-3 vertices to shift
+        return vertices.map((v, i) => {
+            if (Math.random() < 0.5) {
+                if (i < 2) {
+                    v.y += jitter();
+                } else {
+                    v.y -= jitter();
+                }
+
+                if (i == 0 || i == 3) {
+                    v.x += jitter();
+                } else {
+                    v.x -= jitter();
+                }
+            }
+            return v;
+        });
     }
 
     getPolyEdges(p) {
@@ -68,9 +87,10 @@ export default class Drawing {
         const { ctx, gridX, gridY, width, height, gutter } = this;
         ctx.draw(() => {
             ctx.setStyles({ fill: "#efefef" });
+            ctx.setStyles(this.styles);
+
             ctx.rect(this.width, this.height, 0, 0);
 
-            ctx.setStyles(this.styles);
             const cellSizeX = (width - gutter * 2) / gridX;
             const cellSizeY = (height - gutter * 2) / gridY;
 
@@ -90,51 +110,48 @@ export default class Drawing {
 
                     polygons.push(vertices);
                     ctx.polygon(vertices);
-                    ctx.rect(
-                        cellSizeX,
-                        cellSizeY,
-                        gutter + i * cellSizeX,
-                        gutter + j * cellSizeX
-                    );
+                    // ctx.rect(
+                    //     cellSizeX,
+                    //     cellSizeY,
+                    //     gutter + i * cellSizeX,
+                    //     gutter + j * cellSizeX
+                    // );
                 }
             }
 
-            const numLines = gridX * 15;
+            const numLines = gridX * this.linePerCell;
             const step = (width - gutter * 2) / numLines;
             for (let i = 0; i < numLines; i += 1) {
-                const ls = { x: i * step + gutter, y: 0 + gutter };
-                const le = { x: i * step + gutter, y: height - gutter };
-                // ctx.line(ls.x, ls.y, le.x, le.y);
+                const ls = { x: i * step + gutter, y: 0 + gutter - 5 };
+                const le = { x: i * step + gutter, y: height - gutter + 5 };
 
                 const pointsOfIntersections = polygons
-                    .reduce((acc, p) => {
+                    .reduce((acc, p, j) => {
                         const lines = this.getPolyEdges(p);
-                        return acc.concat(
-                            lines.map(({ s, e }) => intersection(s, e, ls, le))
-                        );
+                        const ints = lines.map(({ s, e }) => intersection(s, e, ls, le));
+                        return acc.concat(ints);
                     }, [])
-                    .filter(p => p !== false);
+                    .filter(p => p !== false)
 
                 pointsOfIntersections.forEach((p, i) => {
-                    // ctx.ellipse(3, 3, p.x, p.y);
+                    ctx.ellipse(3, 3, p.x, p.y);
                 });
 
                 pointsOfIntersections.unshift(ls);
                 pointsOfIntersections.push(le);
+                pointsOfIntersections.sort((a, b) => a.y - b.y);
 
-                for (let j = 0; j < pointsOfIntersections.length; j += 2) {
+                for (let j = 0; j < pointsOfIntersections.length - 1; j += 1) {
                     const start = pointsOfIntersections[j];
                     const end = pointsOfIntersections[j + 1];
-                    ctx.line(start.x, start.y, end.x, end.y);
+                    if (j % 2 == 0) {
+                        this.ctx.setStyles({ stroke: "rgba(200, 0, 0, 0.3)" });
+                        ctx.line(start.x, start.y, end.x, end.y);
+                    } else {
+                        this.ctx.setStyles({ stroke: "rgba(0, 0, 200, 0.3)" });
+                    }
                 }
             }
-
-            // const s1 = { x: 250, y: 0 };
-            // const e1 = { x: 250, y: 220 };
-            // const s2 = { x: 100, y: 200 };
-            // const e2 = { x: 280, y: 200 };
-            // ctx.line(s1.x, s1.y, e1.x, e1.y);
-            // ctx.line(s2.x, s2.y, e2.x, e2.y);
         });
     }
 
