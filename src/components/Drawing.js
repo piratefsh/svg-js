92ea9ab539d9c20f433fe692e16f4ad319f20ba4
Sher Minn Chong <sherminn.chong@gmail.com>
2019-01-09 14:17:44 -0500

    Get intersection working

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -1,3 +1,5 @@
+import { intersection } from "../helpers/vector";
+
 export default class Drawing {
     constructor({ styles, ctx, width, height }) {
         // add defaults
@@ -47,6 +49,19 @@ export default class Drawing {
         ];
     }
 
+    getPolyEdges(p) {
+        const edges = [];
+        for (let i = 0; i < p.length - 1; i++) {
+            edges.push({
+                s: p[i],
+                e: p[i + 1]
+            });
+        }
+
+        edges.push({ s: p[p.length - 1], e: p[0] });
+        return edges;
+    }
+
     draw() {
         const { ctx, gridX, gridY, width, height } = this;
         ctx.draw(() => {
@@ -71,10 +86,38 @@ export default class Drawing {
                         }
                     );
 
-                    polygons.push(vertices)
+                    polygons.push(vertices);
                     ctx.polygon(vertices);
                 }
             }
+
+            const numLines = 30;
+            const step = width / numLines;
+            for (let i = 0; i < numLines; i += 1) {
+                const ls = { x: i * step, y: 0 };
+                const le = { x: i * step, y: height };
+                ctx.line(ls.x, ls.y, le.x, le.y);
+
+                const pointsOfIntersections = polygons
+                    .reduce((acc, p) => {
+                        const lines = this.getPolyEdges(p);
+                        return acc.concat(
+                            lines.map(({ s, e }) => intersection(s, e, ls, le))
+                        );
+                    }, [])
+                    .filter(p => p !== false);
+
+                pointsOfIntersections.forEach(p => {
+                    ctx.ellipse(3, 3, p.x, p.y);
+                });
+            }
+
+            // const s1 = { x: 250, y: 0 };
+            // const e1 = { x: 250, y: 220 };
+            // const s2 = { x: 100, y: 200 };
+            // const e2 = { x: 280, y: 200 };
+            // ctx.line(s1.x, s1.y, e1.x, e1.y);
+            // ctx.line(s2.x, s2.y, e2.x, e2.y);
         });
     }
 
