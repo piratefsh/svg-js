d95be19d97dfa7206e3cfeeddaaea2858b02c5e0
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-01 22:00:01 -0400

    Refactor

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -18,6 +18,27 @@ export default class Drawing {
         this.height = height;
     }
 
+    drawRings(offset, angleStep, radius, rings){
+        const points = []
+        const { ctx } = this;
+
+        for(let theta = 0; theta < Math.PI*2; theta += random(0.05, 0.2)){
+            const x = radius.x * Math.sin(theta) * Math.cos(theta/5)
+            const y = radius.y * Math.cos(theta)
+            points.push(translate(rotate({x, y}, Math.PI/4), offset))
+        }
+        points.push(points[0])
+        for(let n = 0; n < rings/2; n++){
+            ctx.startLine();
+            points.forEach((p) => {
+                const x = (p.x - offset.x) * (4*Math.sin(n/rings * Math.PI)) + offset.x
+                const y = (p.y - offset.y) * (4*Math.sin(n/rings * Math.PI)) + offset.y
+                ctx.lineVertex(x, y)
+            })
+            ctx.endLine();
+        }
+    }
+
     draw() {
         const { ctx } = this;
         ctx.draw(() => {
@@ -27,30 +48,14 @@ export default class Drawing {
                 x: this.width/2,
                 y: this.height/2
             }
-            const angleStep = 0.12;
+            const angleStep = 0.1;
             const radius = {
                 x: 40,
                 y: 35
             };
+            const rings = 31;
 
-            const points = []
-
-            for(let theta = 0; theta < Math.PI*2; theta += random(0.05, 0.2)){
-                const x = radius.x * Math.sin(theta) * Math.cos(theta/5) + Math.sin(theta) * random(-4, 3);
-                const y = radius.y * Math.cos(theta) + Math.cos(theta) * random(-4, 3);
-                points.push(translate(rotate({x, y}, Math.PI/4), offset))
-            }
-            points.push(points[0])
-            const rings = 30;
-            for(let n = 0; n < rings/2; n++){
-                ctx.startLine();
-                points.forEach((p) => {
-                    const x = (p.x - offset.x) * (4*Math.sin(n/rings * Math.PI)) + offset.x
-                    const y = (p.y - offset.y) * (4*Math.sin(n/rings * Math.PI)) + offset.y
-                    ctx.lineVertex(x, y)
-                })
-                ctx.endLine();
-            }
+            this.drawRings(offset, angleStep, radius, rings)
         });
     }
 
