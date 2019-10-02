320c3f4c3abd7de9f4acf3db8c9da4c93cb54490
Sher Minn Chong <sherminn@teacherspayteachers.com>
2019-10-02 18:04:35 -0400

    Cleanup

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -24,37 +24,19 @@ export default class Drawing {
         const { ctx, styles } = this;
         ctx.setStyles(styles)
         ctx.draw(() => {
-            // const start = {x: this.width/2, y: this.height/2};
-            // const start = {x: 0, y: 0};
 
-            for(let i = 0; i < 3; i++){
-                const start = {x: 0, y: i * 10};
-                let pos = { x: start.x, y: start.y }
-                ctx.startLine()
-                while(pos.x < this.width && pos.y < this.height){
-                    const newPos = rotate(translate(pos, {x: randomSelect([10, -10]), y: 0}), randomSelect([0, Math.PI/2]), pos)
-                    ctx.lineVertex(newPos.x, newPos.y);
-                    // ctx.ellipse(5, 5, newPos.x, newPos.y);
-                    pos = newPos;
-                }
-                ctx.endLine()
-
+            const start = {x: 0, y: 0};
+            let pos = { x: start.x, y: start.y }
+            ctx.startLine()
+            while(pos.x < this.width && pos.y < this.height){
+                const newPos = rotate(translate(pos, {x: randomSelect([10, -10]), y: 0}), randomSelect([0, Math.PI/2]), pos)
+                ctx.lineVertex(newPos.x, newPos.y);
+                // ctx.ellipse(5, 5, newPos.x, newPos.y);
+                pos = newPos;
             }
+            ctx.endLine()
 
-            // for(let i = 0; i < 80; i++)
-            // {
-            //     const start = {y: 0, x: i * 10};
-            //     let pos = { x: start.x, y: start.y }
-            //     ctx.startLine()
-            //     while(pos.x < this.width && pos.y < this.height){
-            //         const newPos = rotate(translate(pos, {x: 10, y: 0}), randomSelect([0, Math.PI/2]), pos)
-            //         ctx.lineVertex(newPos.x, newPos.y);
-            //         // ctx.ellipse(5, 5, newPos.x, newPos.y);
-            //         pos = newPos;
-            //     }
-            //     ctx.endLine()
 
-            // }
         });
     }
 
