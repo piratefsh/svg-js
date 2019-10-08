c17cb79a55fd90c8aa1239b81a6a544e8021d5a7
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-07 22:47:57 -0400

    Tweaks

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -41,7 +41,7 @@ export default class Drawing {
         ctx.draw(() => {
             const gx = width;
             const gy = height;
-            const nr = 5;
+            const nr = 7;
             const nc = nr;
             const amp = random(0, 2);
             const margin = { x: 50, y: 50 };
@@ -55,45 +55,66 @@ export default class Drawing {
             grid2d(gx, gy, nr, nc, (x, y, cx, cy, i, j) => {
                 const pos = { x, y };
                 // ctx.rect(cx, cy, pos.x, pos.y);
+                const prob = random(0, 1);
                 if (i % 3 == 1) {
-                    const prob = random(0, 1)
-                    if (prob < 0.8) {
+                    if (prob < (i == nr ? 0.3 : 0.8)) {
                         ctx.arc(x, y, cx, cy, Math.PI, 0);
 
-                        if(prob < 0.7){
-                            const len = 1.2
-                            const offset = random(-Math.PI/2 +0.3, Math.PI/2-0.5);
-                            const segment  = {start: Math.PI + len + offset, end: -len + offset}
-                            for (let i = 0; i < 5; i++) {
+                        if (prob < 0.7) {
+                            const len = 1.2;
+                            const offset = random(
+                                -Math.PI / 2 + 0.3,
+                                Math.PI / 2 - 0.5
+                            );
+                            const segment = {
+                                start: Math.PI + len + offset,
+                                end: -len + offset
+                            };
+                            for (let i = 0; i < 6; i++) {
                                 ctx.arc(
                                     x,
                                     y,
                                     cx - 1 * i,
                                     cy - 1 * i,
                                     segment.start,
-                                    segment.end,
+                                    segment.end
                                 );
                             }
                         }
                     }
-                }
-
-                else  {
-                    ctx.arc(x, y, cx, cy, 0, Math.PI);
-                    ctx.arc(x, y, cx-1, cy-1, 0, Math.PI);
-                    if (random(0, 1) < 0.4) {
+                } else {
+                    let angles = { start: 0, end: Math.PI }
+                    let anglesInner = { start: 0.4, end: Math.PI - 0.4 }
+                    if (prob < (i == nr || i == 0 ? 0.3 : 0.8)) {
+                        if(i == 0){
+                            angles = { start: 0, end: Math.PI/2 }
+                        }
+                        if(i == nc){
+                            angles = { start: Math.PI, end: Math.PI/2 }
+                        }
+                        ctx.arc(x, y, cx, cy, angles.start, angles.end);
+                        ctx.arc(x, y, cx - 1, cy - 1, angles.start, angles.end);
+                        if(prob < 0.2){
+                            ctx.arc(x, y, cx - 2, cy - 2, angles.start, angles.end);
+                        }
+                    }
+                    // rings
+                    if (prob < 0.4) {
+                        if(i == 0){
+                            anglesInner = { start: 0.4, end: Math.PI/2 }
+                        }
                         for (let j = 0; j < 5; j++) {
                             ctx.arc(
                                 x,
                                 y,
                                 cx - 4 * j,
                                 cy - 4 * j,
-                                0 + 0.4,
-                                Math.PI - 0.4
+                                anglesInner.start,
+                                anglesInner.end
                             );
                         }
                     }
-                    }
+                }
             });
         });
     }
