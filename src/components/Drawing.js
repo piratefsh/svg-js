09c6c9140c5209ea1a9d6d559a0633d89e86a905
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-09 07:57:00 -0400

    Update

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -9,6 +9,8 @@ import {
 } from "../helpers/math";
 import { grid2d } from "../helpers/grids";
 
+const perm = random(2, 3);
+console.log(perm)
 export default class Drawing {
     constructor({ styles, ctx, width, height }) {
         // add defaults
@@ -43,9 +45,10 @@ export default class Drawing {
             ctx.setStyles(styles);
             const steps = 10;
             const branches = [];
-            const depth = 7;
-            const lineWidth = depth/2;
-            this.branch({ x: width*0.5, y: height }, { y: -160, x: 0 }, lineWidth, depth);
+            const depth = 6;
+            const lineWidth = depth;
+            this.branch({ y: 0, x: height/2 - 30}, { y: 150, x: 0 }, lineWidth, depth);
+            this.branch({ y: width, x: height/2 + 30}, { y: -150, x: 0 }, lineWidth, depth);
         });
     }
 
@@ -55,7 +58,14 @@ export default class Drawing {
         }
     }
 
+    thiccDot(pos, size){
+        for(let i = 0; i < size; i++){
+            this.ctx.ellipse(i, i, pos.x, pos.y)
+        }
+    }
+
     branch(pos, velocity, width, maxSteps) {
+        const size = 4;
         const { ctx } = this;
         const { x, y } = pos;
         if (
@@ -64,39 +74,48 @@ export default class Drawing {
             y < 0
         ) {
             // ctx.ellipse(2, 2, x, y)
-            if(random(0, 1) > 1){
-                const size = 3;
-                ctx.ellipse(size, size, x, y-size)
-                ctx.ellipse(size, size, x-size, y)
-                ctx.ellipse(size, size, x+size, y)
-                ctx.ellipse(size, size, x, y-size)
-                ctx.ellipse(size, size, x, y+size)
+            if(random(0, 1) > 0){
+                // this.thiccDot({x, y}, 6)
+                // ctx.ellipse(size, size, x, y-size)
+                // ctx.ellipse(size, size, x-size, y)
+                // ctx.ellipse(size, size, x+size, y)
+                // ctx.ellipse(size, size, x, y-size)
+                // ctx.ellipse(size, size, x, y+size)
             }
             return;
         }
 
         let lineWidth = width > 1 ? width : 1;
 
+        // this.thiccDot({x, y}, 2)
+
         this.thiccLine(pos, translate(pos, velocity), lineWidth);
         this.branch(
-            translate(pos, mult(velocity, 0.5)),
-            rotate(mult(velocity, 0.6), -1 * Math.PI * random(0.08, 0.12)),
+            translate(pos, mult(velocity, 0.45)),
+            rotate(mult(velocity, 0.5), -1 * Math.PI * 0.3),
             lineWidth - 1,
             maxSteps - 1
         );
 
         this.branch(
             translate(pos, mult(velocity, 0.5)),
-            rotate(mult(velocity, 0.7), Math.PI*random(-0.1, 0.2)),
+            rotate(mult(velocity, -0.66), Math.PI * perm),
             lineWidth - 1,
             maxSteps - 1
         );
         this.branch(
             translate(pos, mult(velocity, 1)),
-            rotate(mult(velocity, 0.63), Math.PI*random(-0.025, 0.025)),
+            rotate(mult(velocity, 0.5), Math.PI*0.25),
             lineWidth - 1,
             maxSteps - 1
         );
+
+        // this.branch(
+        //     translate(pos, mult(velocity, 1)),
+        //     rotate(mult(velocity, 0.3), Math.PI*1),
+        //     lineWidth - 1,
+        //     maxSteps - 1
+        // );
     }
 
     save() {
