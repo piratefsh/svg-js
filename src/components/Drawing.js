4ed9b13a82e6de5638000e06d21aba723c4563cf
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-10 00:31:04 -0400

    Add steps

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -34,51 +34,43 @@ export default class Drawing {
         const { ctx, styles, width, height } = this;
         ctx.draw(() => {
             ctx.setStyles(styles);
-            const numLines = 17;
+            const numLines = 10;
             const step = width / numLines;
             const offsetX = step/2;
-            const thicx = 1.2
+            const maxThickness = 16;
+            const minThickness = 1;
+            const rows = 10;
             for (let i = 0; i < numLines; i++) {
-                this.thiccLine(
-                    i * step,
-                    (height / numLines) * i,
-                    i * step,
-                    height - (height / numLines) * i,
-                    thicx * (numLines - (i > numLines/2 ? numLines - i+1 : 1 + i))
-                );
-                this.thiccLine(
-                    offsetX + i * step,
-                    i < numLines / 2
-                        ? (height / numLines) * i
-                        : height - (height / numLines) * i,
-                    offsetX + i * step,
-                    0,
-                    thicx * i+1
-                );
+                for(let j = 0; j < rows; j++){
+                    const s1 = translate({
+                        x: i * step,
+                        y: 0,
+                    }, {x: 0, y: height/rows*j})
+                    const e1 = translate({
+                        x: i * step,
+                        y: height/rows,
+                    }, {x: 0, y: height/rows*j})
+                    this.thiccLine(
+                        s1.x + step*(j/rows), s1.y, e1.x + step*(j/rows), e1.y, minThickness + maxThickness - Math.sin(1.3*j/rows * i/numLines*Math.PI)* (maxThickness-minThickness)
+                    );
+                }
 
-                this.thiccLine(
-                    i * step - offsetX,
-                    i > numLines / 2
-                        ? (height / numLines) * i
-                        : height - (height / numLines) * i,
-                    i * step - offsetX,
-                    height,
-                    thicx * numLines - i+1
-                );
             }
         });
     }
 
     thiccLine(sx, sy, ex, ey, lineWidth = 1) {
+        lineWidth = Math.trunc(lineWidth)
         const vec = normalize(rotate({ x: sx - ex, y: sy - ey }, Math.PI / 2));
         for (let i = 0; i < lineWidth; i++) {
+            const offset = mult(vec, i - Math.floor(lineWidth / 2))
             const st = translate(
                 { x: sx, y: sy },
-                mult(vec, i - Math.floor(lineWidth / 2))
+                offset
             );
             const en = translate(
                 { x: ex, y: ey },
-                mult(vec, i - Math.floor(lineWidth / 2))
+                offset
             );
             this.ctx.line(st.x, st.y, en.x, en.y);
         }
