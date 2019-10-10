3110968355f2d4d9df9e8eaa7e4fd9e86364b1e4
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-09 23:31:32 -0400

    Lines

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -38,19 +38,21 @@ export default class Drawing {
             const branches = [];
             const depth = 6;
             const lineWidth = depth;
-
-            for(let i = 0; i < 10; i++){
-                this.thiccLine(i * 20, random(30, height-3), random(i*20, width), height-30, i);
-                this.thiccDot(i * 20, 30, i*2);
+            const numLines = 20
+            const step = width/numLines
+            for(let i = 0; i < numLines; i++){
+                this.thiccLine(i*step, height/numLines*i, i*step, height, numLines-i);
+                this.thiccLine(step/2 + i*step, height/numLines*i + 1, step/2 + i*step, 0, i);
             }
+
         });
     }
 
     thiccLine(sx, sy, ex, ey, lineWidth=1){
         const vec = normalize(rotate({x: sx-ex, y: sy-ey}, Math.PI/2));
-        for(let i = -lineWidth/2; i < lineWidth/2; i++){
-            const st = translate({x: sx, y: sy}, mult(vec, i))
-            const en = translate({x: ex, y: ey}, mult(vec, i))
+        for(let i = 0; i < lineWidth; i++){
+            const st = translate({x: sx, y: sy}, mult(vec, i-Math.floor(lineWidth/2)))
+            const en = translate({x: ex, y: ey}, mult(vec, i-Math.floor(lineWidth/2)))
             this.ctx.line(st.x, st.y, en.x, en.y);
         }
     }
