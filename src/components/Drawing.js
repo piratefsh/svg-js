f1144aa6125e7e6c5b5bec99d183c76e78d10d56
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-20 11:20:26 -0400

    WIP

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -3,6 +3,7 @@ import { mult, chordToRad, dist } from "../helpers/math";
 const SQRT_2 = Math.sqrt(2);
 const PI = Math.PI;
 const TWO_PI = 2 * PI;
+const debug = true;
 
 export default class Drawing {
     constructor({ styles, ctx, width, height }) {
@@ -10,7 +11,7 @@ export default class Drawing {
         this.styles = {
             stroke: "black",
             strokeWidth: 1,
-            fill: "rgba(0, 0, 0, 0.9)",
+            fill: "rgba(0, 0, 0, 0.1)",
             ...styles
         };
 
@@ -27,52 +28,49 @@ export default class Drawing {
             ctx.setStyles(this.styles);
             const pos = mult({ x: width, y: height }, 0.5);
             const len = 150;
-            this.sierCurve(len, pos, 4);
+            ctx.startLine();
+            this.sub(len, pos, 3);
+            ctx.endLine();
         });
     }
 
-    sierSq(len, pos, offset = 0) {
-        const sides = 4;
-        const radius = chordToRad(len, TWO_PI / 4);
+    sub(len, pos, iters = 1, rot = PI/2) {
+        const { ctx } = this;
+
         const points = [];
-        for (let i = 0; i < sides; i++) {
-            const theta = offset + (i / sides) * TWO_PI;
-            points.push({
-                x: pos.x + radius * Math.sin(theta),
-                y: pos.y + radius * Math.cos(theta)
-            });
-        }
-        return points;
-    }
-
-    sierCurve(len, pos, iters = 1, rotation = PI / 4) {
-        const points = this.sierSq(len, pos, rotation);
-        points.forEach((p, i) => {
-            const n = points[(i + 1) % points.length];
-            this.ctx.line(p.x, p.y, n.x, n.y);
-        });
-        if (iters == 1) {
-            return;
-        }
-        const childLen = (len / 4) * SQRT_2;
-
+        const egdes = [];
+        const r = (len * SQRT_2) / 2;
         const sides = 4;
-        const r = len / SQRT_2;
         for (let i = 0; i < sides; i++) {
-            const t = (i / sides) * TWO_PI + rotation;
-            const childPos = {
-                x: r * Math.sin(t) + pos.x,
-                y: r * Math.cos(t) + pos.y
+            const t = (i / sides) * TWO_PI + rot;
+            const p = {
+                x: r * Math.sin(t - PI/2) + pos.x,
+                y: r * Math.cos(t - PI/2) + pos.y
             };
 
-            if (dist(childPos, pos) > 0) {
-                this.sierCurve(
-                    childLen,
-                    childPos,
-                    iters - 1,
-                    rotation + PI / 4
-                );
+            const e = {
+                x: r * Math.sin(t + PI / 4) + pos.x,
+                y: r * Math.cos(t + PI / 4) + pos.y
+            };
+            points.push(p);
+            egdes.push(e);
+
+            if (debug) {
+                ctx.setStyles({ stroke: "pink" });
+                ctx.line(pos.x, pos.y, e.x, e.y);
+                ctx.setStyles(this.styles);
             }
+        }
+
+        if (iters == 1) {
+            points.forEach(p => {
+                ctx.lineVertex(p.x, p.y);
+            });
+        } else {
+            egdes.forEach((p, i) => {
+                ctx.ellipse(i * 5, i * 5, p.x, p.y);
+                this.sub(len / 2, p, iters - 1, rot + (TWO_PI/4) * i);
+            });
         }
     }
 
