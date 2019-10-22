a2604ae4b10b5c6b80d309d9f989fefa668d8ec7
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-21 23:45:29 -0400

    Update

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -7,8 +7,8 @@ export default class Drawing {
         // add defaults
         this.styles = {
             stroke: "hsla(355, 90%, 70%, 0.7)",
-            strokeWidth: 3,
-            fill: "hsla(355, 90%, 10%, 1)",
+            strokeWidth: 1,
+            fill: "hsla(355, 90%, 10%, 0.0)",
             ...styles
         };
 
@@ -22,61 +22,57 @@ export default class Drawing {
     draw() {
         const { ctx, width, height } = this;
         ctx.draw(() => {
-            ctx.setStyles({ fill: this.styles.fill, strokeWidth: 0 });
-            ctx.rect(width, height, 0, 0);
             ctx.setStyles(this.styles);
-            const iters = 3;
-            this.sierCurve(height - 40, iters, 1, {
-                x: 20,
-                y: 20
+            this.curveOfPursuit(6, 6*16, width/2, {
+                x: width/2,
+                y: height/2
             });
+
+            // this.curveOfPursuit(4, 60, width/2, {
+            //     x: width/2,
+            //     y: height/2
+            // }, -1, 'skyblue');
         });
     }
 
-    sierCurve(len, iters, lineWidth = 1, offset = { x: 0, y: 0 }) {
-        const tri1 = [{ x: 0, y: len }, { x: 0, y: 0 }, { x: len, y: 0 }].map(
-            p => translate(p, offset)
-        );
-        const tri2 = [
-            { x: len, y: 0 },
-            { x: len, y: len },
-            { x: 0, y: len }
-        ].map(p => translate(p, offset));
+    // from Curve Stitching by Jon Milligan
+    curveOfPursuit(
+        sectors = 3,
+        granularity = 60,
+        size = this.width,
+        center = { x: this.width / 2, y: this.height / 2 },
+        dir = 1,
+        color = 'deeppink'
+    ) {
+        const { ctx } = this;
+        ctx.setStyles({stroke: color})
+        ctx.ellipse(size, size, center.x, center.y);
+        const l = (size * PI) / granularity;
+        const dogStep = (2 * PI) / sectors;
+        const hareStep = (2 * PI) / granularity;
+        for (let b = -PI; b < PI; b += dogStep) {
+            let dog = { x: 0, y: 0 };
+            for (let t = (PI * 2) / sectors + b; t > b; t -= hareStep) {
+                const hare = {
+                    x: size/2 * Math.sin(t),
+                    y: size/2 * Math.cos(t)
+                };
+                ctx.line(
+                    center.x + dog.x,
+                    center.y + dog.y,
+                    center.x + hare.x,
+                    center.y + hare.y
+                );
 
-        // get points for each half of square recursively
-        const half1 = this.sub(tri1, iters);
-        const half2 = this.sub(tri2, iters);
-
-        //combine points and draw
-        const points = [...half1, ...half2];
-        points.forEach((p, i) => {
-            const n = points[(i + 1) % points.length];
-            this.ctx.thickLine(p.x, p.y, n.x, n.y, lineWidth);
-        });
-    }
-
-    // as described in https://www.youtube.com/watch?v=Ps9itT9KcdM
-    sub(pos, iters = 1) {
-        const [p1, p2, p3] = pos;
-        const points = [];
-
-        // find center of current triangle
-        const centroid = triangleCentroid(...pos);
-        if (iters == 0) {
-            // if recursed all the way down, add point
-            points.push(centroid);
-        } else {
-            // else, subdivide triangle into two right angle triangle
-            // and add the points for each
-            const sub1 = [p1, midpoint(p1, p3), p2];
-            const sub2 = [p2, midpoint(p1, p3), p3];
-            points.push(...this.sub(sub1, iters - 1));
-            points.push(...this.sub(sub2, iters - 1));
+                let h = Math.sqrt(
+                    (hare.x - dog.x) * (hare.x - dog.x) +
+                        (hare.y - dog.y) * (hare.y - dog.y)
+                );
+                dog.x += (l * (hare.x - dog.x)) / h;
+                dog.y += (l * (hare.y - dog.y)) / h;
+            }
         }
-
-        return points;
     }
-
     save() {
         this.ctx.save();
     }
