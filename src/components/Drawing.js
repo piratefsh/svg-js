ff38cfd1a2ec2f8f9cea5bfc9c1dd517f53cedd7
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-24 21:39:17 -0400

    dot 60

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -21,9 +21,12 @@ export default class Drawing {
         const { ctx, width, height } = this;
         ctx.draw(() => {
             ctx.setStyles(this.styles);
-            grid2d(20, 20, width, height, (x, y, cx, cy) => {
-                ctx.ellipse(2, 2, x, y)
-            })
+            const gutter = 60;
+            const num = 40;
+            ctx.rect(width - 1, height - 1, 1, 1);
+            grid2d(width-gutter, height-gutter, num, num, (x, y, cx, cy) => {
+                ctx.ellipse(2, 2, x + cx / 2 + gutter/2, y + cy / 2 + gutter/2);
+            });
         });
     }
     save() {
