65a5569d5e9e69670aee9e75d535b378499e97fe
Sher Minn Chong <sherminn.chong@gmail.com>
2019-10-28 22:16:53 -0400

    Add nested squiggles

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -29,26 +29,28 @@ export default class Drawing {
         const { ctx, width, height } = this;
         ctx.draw(() => {
             ctx.setStyles(this.styles);
-            const thickness = 17;
+            const thickness = 11;
             const numStrokes = 7;
             this.squiggle(
-                { x: 0, y: 0 },
+                { x: 0, y: thickness },
                 { x: width * 0.6, y: height * 0.75 },
                 thickness,
                 numStrokes,
-                "hsla(360, 50%, 50%, 0.5)"
+                "hsla(360, 50%, 50%, 0.5)",
+                -1
             );
             this.squiggle(
-                { x: width * 0.15, y: height * 0.11 },
+                { x: thickness / 2, y: height * 0.11 },
                 { x: width * 0.6, y: height * 0.75 },
-                thickness,
+                thickness: thickness - 0,
                 numStrokes,
-                "hsla(360, 50%, 50%, 0.5)"
+                "hsla(200, 50%, 50%, 0.5)",
+                1
             );
         });
     }
 
-    squiggle(pos, size, thickness, numStrokes, color) {
+    squiggle(pos, size, thickness, numStrokes, color, dir = 1) {
         const { ctx, width, height } = this;
         ctx.setStyles({ stroke: color });
 
@@ -66,39 +68,45 @@ export default class Drawing {
                 y: h + pos.y
             };
 
-            ctx.thickLine(s.x, s.y, e.x, e.y, thickness);
-
             if (i == 0) {
-                ctx.thickLine(s.x, s.y, s.x - r / 2, s.y, thickness);
-            } else if (i % 2 == 0) {
-                ctx.thickArc(
-                    s.x,
-                    s.y - r / 2,
-                    r / 2,
-                    r / 2,
-                    Math.PI * 0.5,
-                    -Math.PI * 0.5,
-                    thickness
-                );
+                if (dir != 1) {
+                    ctx.thickLine(s.x, s.y, e.x + r / 2, e.y, thickness);
+                } else {
+                    ctx.thickLine(s.x - r / 2, s.y, e.x, e.y, thickness);
+                }
             } else {
-                ctx.thickArc(
-                    e.x,
-                    e.y - r / 2,
-                    r / 2,
-                    r / 2,
-                    -Math.PI * 0.5,
-                    Math.PI * 0.5,
-                    thickness
-                );
+                if (i == numStrokes - 1) {
+                    if (dir != 1) {
+                        ctx.thickLine(s.x - r / 2, s.y, e.x, e.y, thickness);
+                    } else {
+                        ctx.thickLine(s.x, s.y, e.x + r / 2, e.y, thickness);
+                    }
+                } else {
+                    ctx.thickLine(s.x, s.y, e.x, e.y, thickness);
+                }
+                if (dir == 1 ? i % 2 == 0 : i % 2 == 1) {
+                    ctx.thickArc(
+                        s.x,
+                        s.y - r / 2,
+                        r / 2,
+                        r / 2,
+                        Math.PI * 0.5,
+                        -Math.PI * 0.5,
+                        thickness
+                    );
+                } else {
+                    ctx.thickArc(
+                        e.x,
+                        e.y - r / 2,
+                        r / 2,
+                        r / 2,
+                        -Math.PI * 0.5,
+                        Math.PI * 0.5,
+                        thickness
+                    );
+                }
             }
         }
-        ctx.thickLine(
-            s.x + size.x - r * 2,
-            s.y,
-            s.x + size.x - r - r / 2,
-            s.y,
-            thickness
-        );
     }
     save() {
         this.ctx.save();
